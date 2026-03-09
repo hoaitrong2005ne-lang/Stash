a8dc17f850b12cc90d6ad4c34de6a7f03682611b
hoaitrong2005ne-lang <hoaitrong2005ne@gmail.com>
2026-03-09 10:24:59 +0700

    Update revenuecat.js

--- revenuecat.js
+++ revenuecat.js
@@ -1,5 +1,3 @@
-var obj = JSON.parse($response.body);
-
 if ($request) {
   let headers = $request.headers;
   delete headers["x-revenuecat-etag"];
@@ -16,14 +14,14 @@ if ($response) {
 
   obj.subscriber.subscriptions["locket_gold"] = {
     "expires_date": "2099-12-31T23:59:59Z",
-    "purchase_date": "2022-01-01T00:00:00Z",
+    "purchase_date": "2023-01-01T00:00:00Z",
     "store": "app_store"
   };
 
   obj.subscriber.entitlements["Gold"] = {
     "expires_date": "2099-12-31T23:59:59Z",
     "product_identifier": "locket_gold",
-    "purchase_date": "2022-01-01T00:00:00Z"
+    "purchase_date": "2023-01-01T00:00:00Z"
   };
 
   $done({ body: JSON.stringify(obj) });

e28a5d6d5dea6e59f6d4e3e1325347ff20ea5539
hoaitrong2005ne-lang <hoaitrong2005ne@gmail.com>
2026-03-09 10:14:56 +0700

    Update revenuecat.js

--- revenuecat.js
+++ revenuecat.js
@@ -1,19 +1,30 @@
 var obj = JSON.parse($response.body);
 
-obj.subscriber = obj.subscriber || {};
-obj.subscriber.subscriptions = obj.subscriber.subscriptions || {};
-obj.subscriber.entitlements = obj.subscriber.entitlements || {};
+if ($request) {
+  let headers = $request.headers;
+  delete headers["x-revenuecat-etag"];
+  delete headers["X-RevenueCat-ETag"];
+  $done({ headers });
+}
 
-obj.subscriber.subscriptions["locket_gold"] = {
-  "expires_date": "2099-12-31T23:59:59Z",
-  "purchase_date": "2022-01-01T00:00:00Z",
-  "store": "app_store"
-};
+if ($response) {
+  let obj = JSON.parse($response.body);
 
-obj.subscriber.entitlements["Gold"] = {
-  "expires_date": "2099-12-31T23:59:59Z",
-  "product_identifier": "locket_gold",
-  "purchase_date": "2022-01-01T00:00:00Z"
-};
+  obj.subscriber = obj.subscriber || {};
+  obj.subscriber.subscriptions = obj.subscriber.subscriptions || {};
+  obj.subscriber.entitlements = obj.subscriber.entitlements || {};
 
-$done({ body: JSON.stringify(obj) });
+  obj.subscriber.subscriptions["locket_gold"] = {
+    "expires_date": "2099-12-31T23:59:59Z",
+    "purchase_date": "2022-01-01T00:00:00Z",
+    "store": "app_store"
+  };
+
+  obj.subscriber.entitlements["Gold"] = {
+    "expires_date": "2099-12-31T23:59:59Z",
+    "product_identifier": "locket_gold",
+    "purchase_date": "2022-01-01T00:00:00Z"
+  };
+
+  $done({ body: JSON.stringify(obj) });
+}
